031e5d7d080df65763b5da2a64e3739cf30fef64
sanat-k1 <sanatk791@gmail.com>
2024-09-08 19:47:44 +0530

    deploy6

--- server/app.cjs
+++ server/app.cjs
@@ -8,7 +8,7 @@ const session = require('express-session')
 
 const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/WEB';
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
